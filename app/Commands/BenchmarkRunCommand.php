39a22061d4f033c961d1f904298204dcb787ea4d
brendt <brent.roose@gmail.com>
2026-02-27 09:23:52 +0100

    wip

--- app/Commands/BenchmarkRunCommand.php
+++ app/Commands/BenchmarkRunCommand.php
@@ -478,7 +478,7 @@ final class BenchmarkRunCommand
             exec($gitCommit, $output, $returnCode);
 
             if ($returnCode !== 0) {
-                $this->prError($prNumber, "Nothing to commit");
+                $this->prWarning($prNumber, "Nothing to commit");
                 return;
             }
         }
